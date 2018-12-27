5ca4e1955821f76b10ef2608be7d299fe4b17496
Rodrigo Ferreira <rdferreira90@gmail.com>
2018-12-27 09:54:53 -0200

    Produto e assiciacao muitos para muitos

---
+++ src/main/java/com/rodrigoferreira/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.rodrigoferreira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigoferreira.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+	}
+
+
